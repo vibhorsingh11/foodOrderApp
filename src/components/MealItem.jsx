13d9dd586366f54f635650d723ebf06a437542b9
vibhorsingh11 <vibhorsinghcs@gmail.com>
2024-04-17 19:39:45 +0530

    added individual meal and custom button comp

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,6 @@
+import { currencyFormatter } from '../util/formatting';
+import Button from './UI/Button';
+
 export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
@@ -6,11 +9,13 @@ export default function MealItem({ meal }) {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
