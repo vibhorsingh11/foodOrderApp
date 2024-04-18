0a67f9d8128939360f78fd854d6c30b24014dc51
vibhorsingh11 <vibhorsinghcs@gmail.com>
2024-04-18 16:03:17 +0530

    added cartItem component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,8 @@ import CartContext from '../store/CartContext.jsx';
 import Modal from './UI/Modal.jsx';
 import { currencyFormatter } from '../util/formatting';
 import UserProgressContext from '../store/UserProgressContext.jsx';
+import CartItem from './CartItem.jsx';
+import Button from './UI/Button.jsx';
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -23,17 +25,24 @@ export default function Cart() {
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <button textOnly onClick={handleHideCart}>
+        <Button textOnly onClick={handleHideCart}>
           Close
-        </button>
-        <button onClick={handleHideCart}>Go to Checkout</button>
+        </Button>
+        {cartCtx.items.length > 0 && (
+          <Button onClick={handleHideCart}>Go to Checkout</Button>
+        )}
       </p>
     </Modal>
   );
